1ea776686d083f593d38549be56ee2a89550daa0
kkumarM <karthikkumaar.m@gmail.com>
2022-04-11 00:50:49 +0530

    Added exec_run to perform bom generation inside container

--- run.py
+++ run.py
@@ -6,10 +6,13 @@ import docker
 import subprocess
 import os
 from termcolor import colored
+from io import StringIO,BytesIO
+import tarfile
 
 class GenerateBOM(object):
 	def __init__(self):
 		self.client = docker.from_env()
+		self.obj_id = ""
 
 	def get_running_containers(self):
 		"""
@@ -23,16 +26,39 @@ class GenerateBOM(object):
 		container_name = [container.name for container in running_containers]
 		return container_name, container_ID, running_containers 
 
-	def create_base_package(self):
+	def create_base_package(self, image, name):
 		"""
 		Function to create base package used in docker images
 		Parameters : None
 		Return: 
 		"""
-		new_base_package = input(colored("Enter base image package used in the docker image:","gray"))
-		name = new_base_package.replace('.','_').replace(':','_').replace('/','_')
-		temp_cont = self.client.create_container(image=new_base_package,command = "dpkg-query -W -f='${package}\n' > {0}}.txt".format(name))
-		container.exec_run()
+		temp_cont = self.client.create_container(image=image, command = "dpkg-query -W -f='${package}\n' > {0}}.txt".format(name))
+		container.exec_run(cmd,stream=True,demux=False,detach=False,user="root")
+		
+
+	def run_bom_generator(self):
+		cmd = "/bin/bash -c 'whoami && apt-get update && apt-get install git'"
+		#cmd1 = "/bin/bash -c '[ -d './BOM_generator' ] && rm -rf ./BOM_generator'"
+		cmd2 = "/bin/bash -c 'git clone https://github.com/kkumarM/BOM_generator.git'"
+		cmd3 = f"/bin/bash -c 'cd ./BOM_generator && python3 bom_script_new.py {base_package}.txt'"
+		print(self.obj_id)
+		_,out = self.obj_id.exec_run(cmd,stream=True,demux=False,detach=False,user="root")
+		#_,out1 = self.obj_id.exec_run(cmd1,stream=True,demux=False,detach=False,user="root")
+		#_,out2 = self.obj_id.exec_run(cmd2,stream=True,demux=False,detach=False,user="root")
+		_,out3 = self.obj_id.exec_run(cmd3,stream=True,demux=False,detach=False,user="root")
+		# for data in out:
+		# 	print(data.decode("utf-8"))
+		# for data in out2:
+		# 	print(data.decode("utf-8"))
+		for data in out3:
+			print(data.decode("utf-8"))
+		res,stat = self.obj_id.get_archive('BOM_generator/CSV_outputs', chunk_size=2097152, encode_stream=False)
+		# Save Output files from container to local
+		filetype = BytesIO(b"".join(b for b in res))
+		tar = tarfile.open(fileobj=filetype)
+		tar.extractall("./")
+		return ("Files Saved in current Directory")
+
 
 	def get_user_inputs(self):
 		"""
@@ -41,7 +67,6 @@ class GenerateBOM(object):
 		Parameters : None
 		Return: 
 		"""
-		global obj_id
 		container_name, container_id, running_containers = self.get_running_containers()
 		print('\n',colored("List of Running Containers:", "green"),'\n')
 		for i,j in zip(container_id,container_name):
@@ -53,27 +78,28 @@ class GenerateBOM(object):
 				pass
 			else:
 				print(colored("Container ID doesn't exists, Please verify !", "red"))
-
+				input_container_id = input(colored("Enter Container ID:","yellow"))
 			for obj in running_containers:
 				if input_container_id == obj.short_id:
-					obj_id = obj
-			base_package = input(colored("Enter Base Package (eg: openvino/ubuntu18_runtime:2021.2):","yellow"))
-			base_package = base_package.replace('.','_').replace(':','_').replace('/','_')
+					self.obj_id = obj
+			image = input(colored("Enter Base Package (eg: openvino/ubuntu18_runtime:2021.2):","yellow"))
+			base_package = image.replace('.','_').replace(':','_').replace('/','_')
 			if os.path.exists(f"base_image_packages/{base_package}.txt"):
 				print("Inside")
 				print(colored("Base package already present !","cyan"))	
-				cmd = 'python3 bom_script_new.py'
-				print(obj_id)
-				out = obj_id.exec_run('/bin/sh -c ls',stream=True, demux=False)
-				# print(stderr)
-				# print(stdout)
-				print(out.output)
+				self.run_bom_generator(base_package, new_base_image=False)
+
 			else:
 				print(colored("Base package not present !","cyan"))	
 				out = input(colored("Do you want to create one (yes/no):","cyan"))
+				if out == "yes" or out == "y":
+					print(base_package)
+					self.create_base_package(image, base_package)
+					self.run_bom_generator(base_package, new_base_image=True)
+				else:
+					print(colored("Exiting....","red"))
 				# if out == "yes":
-				# 	self.create_base_package()
-				
+				# 	self.create_base_package()			
 			
 		except Exception as e:
 			print(e.__class_)
